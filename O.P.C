7bad43567a9125577c61b629ef80d2b2330e314d
Saif Fouad Saif ElNaser Zedan ElSaaedy <saiflion10000@gmail.com>
2018-10-20 01:47:55 +0200

    add database and cs project

---
+++ O.P.C
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: saif
+ * Date: 10/19/2018
+ * Time: 5:56 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SS
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
