7ce4928eafd2c4144b3b6138acb5e47a40a10d0e
Your Name <you@example.com>
2018-11-14 04:42:42 +0200

    add a database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,5 @@
-create database OPC
-create table storage (id int primary key identity (1,1) , ultrasonicFK int , rightmotorFK int , leftmotorFK int )
-create table ultrasonic (id int primary key identity (1,1) , ultrasonic float)
-create table [right] (id int primary key identity (1,1) ,speed float)
-create table [left] (id int primary key identity (1,1) ,speed float)
+create proc add_storage
+as
+begin
 
+end
